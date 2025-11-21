76c1115f9b75f4825843c192c8a6c7b0b4a941e4
korchiabdelaziz <abdelazizkorchi03@gmail.com>
2025-11-21 12:11:03 +0100

    first push

---
+++ vote-service/src/test/java/evotingsystem/voteservice/VoteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package evotingsystem.voteservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoteServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ voter-service/src/main/java/evotingsystem/voterservice/VoterServiceApplication.java
@@ -0,0 +1,13 @@
+package evotingsystem.voterservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoterServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VoterServiceApplication.class, args);
+    }
+
+}
